0ddc80486fb7e14921f7a620c53b01bad787f5b9
Yazmin Perez <zoeperez333@gmail.com>
2022-07-01 14:27:23 -0700

    change type to kind and state to statecode

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,4 +1,4 @@
 class Park < ApplicationRecord
   validates :name, :longitude, :latitude, :statecode, :animal, :description, presence: :true
-  # validates :type, inclusion: { in: [true, false] }
+  validates_inclusion_of :kind, in: ['national', 'state']
 end
