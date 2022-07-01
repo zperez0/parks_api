487b6fd18a6c568e4e07335562775fa9a66a6f73
Yazmin Perez <zoeperez333@gmail.com>
2022-07-01 14:01:09 -0700

    change state to statecode

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+class Seed
+  def self.begin
+    seed = Seed.new
+    seed.generate_parks
+  end
+
+  def generate_parks
+    20.times do |i|
+      type_of = ["national", "state"]
+      park = Park.create!(
+        name: Faker::Games::Pokemon.move,
+        longitude: Faker::Address.longitude,
+        latitude: Faker::Address.latitude,
+        statecode: Faker::Address.state,
+        animal: Faker::Games::Pokemon.name,
+        type: type_of.sample,
+        description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
+      )
+      puts " Park #{i}: Name is #{park.name}, Longitude is #{park.longitude}, Latitude is #{park.latitude}, State is #{park.state}, Animal is #{park.animal}, and Type is #{park.type}, Description is #{park.description}"
+    end
+  end
+end
+
+Seed.begin
