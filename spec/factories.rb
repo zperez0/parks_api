446cea324ae614780440bb3493b7163d4528b5c3
Yazmin Perez <zoeperez333@gmail.com>
2022-07-01 15:00:34 -0700

    add and setup factories.rb

---
+++ spec/factories.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory(:park) do
+    name { Faker::Games::Pokemon.move }
+    longitude { Faker::Address.longitude }
+    latitude { Faker::Address.latitude }
+    statecode { Faker::Address.state_abbr }
+    animal { Faker::Games::Pokemon.name }
+    kind { type_of.sample }
+    description { Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4) }
+  end
+end
