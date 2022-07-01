05959fed9c307a60940816cc24bebd1cdbf85f51
Yazmin Perez <zoeperez333@gmail.com>
2022-07-01 16:21:33 -0700

    add park_type variable

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,12 @@
 FactoryBot.define do
   factory(:park) do
+    park_type = ["national", "state"]
     name { Faker::Games::Pokemon.move }
     longitude { Faker::Address.longitude }
     latitude { Faker::Address.latitude }
     statecode { Faker::Address.state_abbr }
     animal { Faker::Games::Pokemon.name }
-    kind { type_of.sample }
+    kind { park_type.sample }
     description { Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4) }
   end
 end
