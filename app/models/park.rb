a92b0b160632e006ad06d7c933f93dfff6ecc049
Yazmin Perez <zoeperez333@gmail.com>
2022-07-02 13:33:40 -0700

    add default_scope

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,12 +1,14 @@
 class Park < ApplicationRecord
+
   validates :name, :longitude, :latitude, :statecode, :animal, :description, presence: :true
-  validates_inclusion_of :kind, in: ['national', 'state']
+  validates_inclusion_of :kind, in: ["national", "state"]
   before_save :titleize_park_name
 
-
+  default_scope { order(name: :asc) }
 
   private
+
   def titleize_park_name
-  self.name = self.name.titleize
+    self.name = self.name.titleize
   end
 end
