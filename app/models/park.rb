37ab169d0463d150147b5acc5625ef146fd238f7
Yazmin Perez <zoeperez333@gmail.com>
2022-07-02 12:38:36 -0700

    add titleize_park_name()

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,4 +1,12 @@
 class Park < ApplicationRecord
   validates :name, :longitude, :latitude, :statecode, :animal, :description, presence: :true
   validates_inclusion_of :kind, in: ['national', 'state']
+  before_save :titleize_park_name
+
+
+
+  private
+  def titleize_park_name
+  self.name = self.name.titleize
+  end
 end
