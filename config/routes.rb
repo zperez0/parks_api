1911f5980febff4a71a266e1c7ef6d4e8db40502
Yazmin Perez <zoeperez333@gmail.com>
2022-07-01 19:10:33 -0700

    fix spacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'parks#index'
-  resources :parks
+  namespace :api do
+    namespace :v1 do
+      resources :parks
+    end
+    namespace :v2 do
+      resources :parks
+    end
+  end
 end
