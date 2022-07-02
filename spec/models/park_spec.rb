6aae5f8a46834f170698867cc63711dac97b0766
Yazmin Perez <zoeperez333@gmail.com>
2022-07-02 12:38:17 -0700

    add testing for titleize_park_name()

--- spec/models/park_spec.rb
+++ spec/models/park_spec.rb
@@ -9,3 +9,10 @@ describe Park do
   it { should validate_inclusion_of(:kind).in_array(['national', 'state']) }
   it { should validate_presence_of :description }
 end
+
+describe Park do
+  it("titleizes the name of a park")do
+    park = Park.create!({name: "forest", longitude: 123, latitude: 456, statecode: "FL", animal: "test_animal", kind: "state", description: "test_description" })
+    expect(park.name).to eq "Forest"
+  end
+end
